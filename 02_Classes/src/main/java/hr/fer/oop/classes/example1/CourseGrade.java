a660b3af50a5fecce15a8ec0ed9b140573799ac9
boris612 <boris.milasinovic@fer.hr>
2016-09-27 22:50:28 +0200

    Examples for the second lecture: Classes - abstraction and encapsulation

---
+++ 02_Classes/src/main/java/hr/fer/oop/classes/example1/CourseGrade.java
@@ -0,0 +1,7 @@
+package hr.fer.oop.classes.example1;
+
+class CourseGrade {
+	int code;
+	String title;
+	int grade;
+}
---
+++ 02_Classes/src/main/java/hr/fer/oop/classes/example2/CourseGrade.java
@@ -0,0 +1,9 @@
+package hr.fer.oop.classes.example2;
+
+public class CourseGrade {
+	public int code;
+	public String title;
+	public int grade;
+	protected int temp_protected; /* dodano da se u dijagramu klasa vidi razlika */
+	int temp_package;  /* dodano da se u dijagramu klasa vidi razlika */
+}
