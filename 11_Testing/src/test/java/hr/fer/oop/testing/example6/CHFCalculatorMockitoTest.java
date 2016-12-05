921008e2287ad15de2ca86f11a71b6bbf99c2a1f
boris612 <boris.milasinovic@fer.hr>
2016-12-05 19:21:29 +0100

    Updated import for  MockitoJUnitRunner

--- 11_Testing/src/test/java/hr/fer/oop/testing/example6/CHFCalculatorMockitoTest.java
+++ 11_Testing/src/test/java/hr/fer/oop/testing/example6/CHFCalculatorMockitoTest.java
@@ -11,7 +11,7 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CHFCalculatorMockitoTest {
