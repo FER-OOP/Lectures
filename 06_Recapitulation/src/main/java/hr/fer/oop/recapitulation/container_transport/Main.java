61ed577ea163722d3781262429804b6b1d5c9c78
boris612 <boris.milasinovic@fer.hr>
2017-10-24 10:08:16 +0200

    Print containers weights

--- 06_Recapitulation/src/main/java/hr/fer/oop/recapitulation/container_transport/Main.java
+++ 06_Recapitulation/src/main/java/hr/fer/oop/recapitulation/container_transport/Main.java
@@ -8,13 +8,16 @@ public class Main {
         Container container = new Container(10, 100, 80, 3);
         System.out.println(container.add(boxedCargo1));//true
         System.out.println(container.add(boxedCargo2));//false
+        System.out.println(container.getWeight());//10 + 32.5 -> 42.5
         
         System.out.println(container.remove(boxedCargo2));//false
         System.out.println(container.remove(boxedCargo1));//true
+        System.out.println(container.getWeight());//10
         
         CargoHolder truck = new BoxedCargoTruck(8.2, 60);
         System.out.println(truck.add(boxedCargo1));//true
         System.out.println(truck.add(boxedCargo2));//true
+        System.out.println(truck.getWeight());//8.2 + 32.5 + 18.9 -> 59.6
         
         CargoHolder ship = new ContainerShip(120.3, 567.2, 60);
         System.out.println(ship.add(container));//true
