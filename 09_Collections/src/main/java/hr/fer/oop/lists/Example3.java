5233b0125eab1a45d5f4b25d35e64fc023730dee
Boris Milašinović <boris.milasinovic@fer.hr>
2017-11-12 12:38:55 +0100

    Collection/Example 3:some minor concept changes

--- 09_Collections/src/main/java/hr/fer/oop/lists/Example3.java
+++ 09_Collections/src/main/java/hr/fer/oop/lists/Example3.java
@@ -1,7 +1,9 @@
 package hr.fer.oop.lists;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Predicate;
@@ -9,23 +11,9 @@ import java.util.function.Predicate;
 public class Example3 {
 
 	public static void main(String[] args) {
-
-		Scanner scanner = new Scanner(System.in);
-		List<Double> numbers = new ArrayList<>();
 		
-		System.out.println("Enter numbers (quit for end):");
-		while(true) {
-			if(scanner.hasNextDouble()) {
-				numbers.add(scanner.nextDouble());
-				continue;
-			}
-			String value = scanner.next();
-			if("quit".equals(value)) {
-				break;
-			}
-			System.out.println("Unexpected input: " + value);
-		}
-		
+		List<Double> numbers = LoadNumbers();
+						
 		if(!numbers.isEmpty()) {
 			double avg = calcAverage(numbers);
 			double limit = avg*1.2;
@@ -33,6 +21,7 @@ public class Example3 {
 			//remove all lower than limit
 			Predicate<Double> belowLimit = new BelowLimit(limit);
 			numbers.removeIf(belowLimit);
+			
 			//sort remaining elements
 			Collections.sort(numbers);
 			
@@ -41,12 +30,29 @@ public class Example3 {
 				System.out.println(number);			
 			}
 			
-		}
-		
-		scanner.close();
+		}				
 	}
 
-	private static double calcAverage(List<Double> numbers) {
+	private static List<Double> LoadNumbers() {
+		List<Double> numbers = new LinkedList<>();
+		try(Scanner scanner = new Scanner(System.in)){					
+			System.out.println("Enter numbers (quit for end):");
+			while(true) {
+				if(scanner.hasNextDouble()) {
+					numbers.add(scanner.nextDouble());
+					continue;
+				}
+				String value = scanner.next();
+				if("quit".equals(value)) {
+					break;
+				}
+				System.out.println("Unexpected input: " + value);
+			}
+		}
+		return numbers;
+	}
+
+	private static double calcAverage(Collection<Double> numbers) {
 		double sum = 0;
 		for(double d : numbers) {
 			sum += d;
