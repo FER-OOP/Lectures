b5d4d4bd597db26bebd19b84fef6e7ae0249a9f6
Boris Milašinović <boris.milasinovic@fer.hr>
2019-03-26 19:26:20 +0100

    finally and exception propagation

---
+++ 07_Exceptions/src/main/java/hr/fer/oop/exceptions/example4/ExampleWithFinally.java
@@ -0,0 +1,46 @@
+package hr.fer.oop.exceptions.example4;
+
+public class ExampleWithFinally {
+
+	private static void m2() {
+		System.out.println("\t\tHello from m2");
+		try {
+			int num = Integer.parseInt("ABC"); 
+			System.out.println("M2-This command is never run");		
+		}
+		catch(ArrayIndexOutOfBoundsException exc) {
+			//catching "wrong exception";
+			System.out.println("Catch in m2");
+		}
+		finally {
+			System.out.println("\t\tFinally-M2");
+		}
+		
+		System.out.println("\t\tEnd of m2");
+	}
+	
+	private static void m1() {
+		System.out.println("\tCalling m2 from m1");
+		try {
+			m2();	
+			System.out.println("M1-This command is never run");		
+		}
+		catch(NumberFormatException exc) {			
+			System.out.println("\tCatch in m1 - printing Stack trace");
+			exc.printStackTrace(System.out);
+			System.out.println();
+		}
+		finally {
+			System.out.println("\tFinally-M1");
+		}
+		System.out.println("\tEnd of m1");
+	}	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Before m1");
+		m1();
+		System.out.println("After m1");				
+	}
+
+}
