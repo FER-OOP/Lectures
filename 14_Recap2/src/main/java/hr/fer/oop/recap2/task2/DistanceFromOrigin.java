9515ff00ed269495c50dcd0aea656b6c07cee07e
Boris Milasinovic <boris.milasinovic@fer.hr>
2019-05-22 12:46:35 +0200

    14_Recap2_task2

---
+++ 14_Recap2/src/main/java/hr/fer/oop/recap2/task2/DistanceFromOrigin.java
@@ -0,0 +1,15 @@
+package hr.fer.oop.recap2.task2;
+
+import java.util.function.Function;
+
+public class DistanceFromOrigin implements Function<Pair<? extends Number>, Double> {
+
+	@Override
+	public Double apply(Pair<? extends Number> t) {
+		double distance = Math.sqrt(
+				Math.pow(t.getX().doubleValue(), 2) 
+			  + Math.pow(t.getY().doubleValue(), 2));
+		return distance;
+	}
+
+}
---
+++ 14_Recap2/src/main/java/hr/fer/oop/recap2/task2/Point.java
@@ -0,0 +1,9 @@
+package hr.fer.oop.recap2.task2;
+
+public class Point extends Pair<Integer> {
+
+	public Point(int x, int y) {
+		super(x, y);		
+	}
+
+}
