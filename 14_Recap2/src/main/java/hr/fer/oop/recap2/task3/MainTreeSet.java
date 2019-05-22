1577049c84f2638fac4bb4a821b41e982e8dbd5a
Boris Milasinovic <boris.milasinovic@fer.hr>
2019-05-22 12:59:51 +0200

    14_Recap2_task3

---
+++ 14_Recap2/src/main/java/hr/fer/oop/recap2/task3/MainTreeSet.java
@@ -0,0 +1,33 @@
+package hr.fer.oop.recap2.task3;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import hr.fer.oop.recap2.task2.Point;
+
+public class MainTreeSet {
+
+	public static void main(String[] args) {
+		Comparator<Point> comp = new PointComparator();
+		Set<Point> set = new TreeSet<>(comp);
+		addToSet(set);										
+		System.out.println("Set with distance comparator:\n" + set);
+		
+		set = new TreeSet<>();
+		addToSet(set);										
+		System.out.println("Set with natural order:\n" + set);
+
+	}
+
+	private static void addToSet(Set<Point> set) {
+		set.add(new Point(-5, 12));
+		set.add(new Point(3, -4));
+		set.add(new Point(12, 9));
+		set.add(new Point(3, 4));
+		set.add(new Point(4, 3));
+		set.add(new Point(-9, 12));
+		set.add(new Point(-5, -12));			
+	}
+
+}
---
+++ 14_Recap2/src/main/java/hr/fer/oop/recap2/task3/MainTreeSetCompositeComparator.java
@@ -0,0 +1,29 @@
+package hr.fer.oop.recap2.task3;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import hr.fer.oop.recap2.task2.Point;
+
+public class MainTreeSetCompositeComparator {
+
+	public static void main(String[] args) {
+		Comparator<Point> comp = new PointComparator();
+		comp = comp.thenComparing(Comparator.naturalOrder());
+		Set<Point> set = new TreeSet<>(comp);
+		addToSet(set);										
+		System.out.println("Composite comparator:\n" + set);		
+	}
+
+	private static void addToSet(Set<Point> set) {
+		set.add(new Point(-5, 12));
+		set.add(new Point(3, -4));
+		set.add(new Point(12, 9));
+		set.add(new Point(3, 4));
+		set.add(new Point(4, 3));
+		set.add(new Point(-9, 12));
+		set.add(new Point(-5, -12));			
+	}
+
+}
