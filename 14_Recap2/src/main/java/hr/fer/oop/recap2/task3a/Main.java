af198ef309bd7f156b6d55dee9482e91dc6fdc25
Boris Milasinovic <boris.milasinovic@fer.hr>
2019-05-23 12:55:00 +0200

    Tasks 3a and 3b, StreamAPI

---
+++ 14_Recap2/src/main/java/hr/fer/oop/recap2/task3a/Main.java
@@ -0,0 +1,36 @@
+package hr.fer.oop.recap2.task3a;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import hr.fer.oop.recap2.task2.DistanceFromOrigin;
+import hr.fer.oop.recap2.task2.Point;
+import hr.fer.oop.recap2.task2.QuadrantPredicate;
+import hr.fer.oop.recap2.task3.PointComparator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Point> list = new ArrayList<>();
+		list.add(new Point(-5, 12));
+		list.add(new Point(3, -4));
+		list.add(new Point(12, 9));
+		list.add(new Point(3, 4));
+		list.add(new Point(4, 3));
+		list.add(new Point(-9, 12));
+		list.add(new Point(-5, -12));				
+		
+		DistanceFromOrigin dist = new DistanceFromOrigin();
+		
+		List<String> newlist = list.stream()								
+								.filter(new QuadrantPredicate(true, true, false, true))
+								.sorted(new PointComparator().reversed())
+								.map(p -> String.format("dist(%s) = %.2f",
+											p, dist.apply(p)
+										))
+								.collect(Collectors.toList());
+		System.out.println(newlist);			
+	}
+
+}
---
+++ 14_Recap2/src/main/java/hr/fer/oop/recap2/task3b/Main.java
@@ -0,0 +1,34 @@
+package hr.fer.oop.recap2.task3b;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+
+import hr.fer.oop.recap2.task2.DistanceFromOrigin;
+import hr.fer.oop.recap2.task2.Point;
+import hr.fer.oop.recap2.task2.QuadrantPredicate;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Point> list = new ArrayList<>();
+		list.add(new Point(-5, 12));
+		list.add(new Point(3, -4));
+		list.add(new Point(12, 9));
+		list.add(new Point(3, 4));
+		list.add(new Point(4, 3));
+		list.add(new Point(-9, 12));
+		list.add(new Point(-5, -12));				
+		
+		DistanceFromOrigin dist = new DistanceFromOrigin();
+		
+		OptionalDouble avg = list.stream()								
+								.filter(new QuadrantPredicate(true, true, false, true))
+								.mapToDouble(p -> dist.apply(p))
+								.average();
+		avg.ifPresentOrElse(
+				val -> System.out.println("Avg = " + val), 
+				() -> System.out.println("No data for selected quadrants")
+			);			
+	}
+}
