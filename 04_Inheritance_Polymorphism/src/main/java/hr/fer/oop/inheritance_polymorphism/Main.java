0488d02537ab4a54ef39760fee401035721223a4
Boris Milasinovic <boris.milasinovic@fer.hr>
2020-03-14 16:43:50 +0100

    minor changes

--- 04_Inheritance_Polymorphism/src/main/java/hr/fer/oop/inheritance_polymorphism/Main.java
+++ 04_Inheritance_Polymorphism/src/main/java/hr/fer/oop/inheritance_polymorphism/Main.java
@@ -5,8 +5,8 @@ import java.time.LocalDate;
 public class Main {
 
 	public static void main(String[] args) {
-		Item item = new Item("1256", "Rambo keks");
-		item.setNetSalePrice(5);
+		Item item = new Item("1256", "Domaćica");
+		item.setNetSalePrice(10);
 		System.out.format("%s, price: %.2f, type: %s%n", item, item.getPrice(1), item.getItemType());		
 		Food food = new Food("777", "CaoCao", 2.5, LocalDate.of(2016,5, 11));
 		System.out.format("%s, price: %.2f, type: %s%n", food, food.getPrice(1), food.getItemType());
