945f74a0201d92ca874a330a3230e89b74bb1d65
MarioKusek <mario.kusek@fer.hr>
2020-04-06 14:47:53 +0200

    Updated corner conditions

--- Exercises/05_Recapitulation/src/main/java/hr/fer/oop/doodle/Doodle.java
+++ Exercises/05_Recapitulation/src/main/java/hr/fer/oop/doodle/Doodle.java
@@ -7,28 +7,23 @@ import hr.fer.oop.list.SimpleArrayList;
 /**
  * Task 2
  *
- * An arbitrary number need to fill in a Doodle in an attempt to meet during
- * the current day. The Doodle needs to ask them to fill in their names and the
+ * An arbitrary number need to fill in a Doodle in an attempt to meet during the
+ * current day. The Doodle needs to ask them to fill in their names and the
  * desired time of the meeting or the keyword "END" via the console, and print
  * their responses in a tabular format, e.g:
  *
- * |  ----    |Time      |
- * |Alice     |12        |
- * |Bob       |13        |
- * |Charile   |14        |
+ * | ---- |Time | |Alice |12 | |Bob |13 | |Charile |14 |
  *
  * It is necessary to use {@link hr.fer.oop.list.SimpleArrayList} from Task 1
  *
  * Zadatak 2
  *
- * Proizvoljan broj osoba trebaj popuniti Doodle u pokušaju da se sastanu u toku dana.
- * Doodle ih treba pitati da navedu imena, željeno vrijeme sastanka ili ključnu riječ
- * "END" te treba ispisati njihove odgovore u tabličnom obliku, npr. :
+ * Proizvoljan broj osoba trebaj popuniti Doodle u pokušaju da se sastanu u toku
+ * dana. Doodle ih treba pitati da navedu imena, željeno vrijeme sastanka ili
+ * ključnu riječ "END" te treba ispisati njihove odgovore u tabličnom obliku,
+ * npr. :
  *
- * |  ----    |Time      |
- * |Alice     |12        |
- * |Bob       |13        |
- * |Charile   |14        |
+ * | ---- |Time | |Alice |12 | |Bob |13 | |Charile |14 |
  *
  * Potrebno je koristiti {@link SimpleArrayList} iz Zadatka 1
  *
@@ -37,72 +32,78 @@ import hr.fer.oop.list.SimpleArrayList;
  */
 
 public class Doodle {
+  private static final String DOODLE_FORMAT = "|%-10s|%-10s|%n";
+  private static final String EOI = "END";
 
-	private static final String EOI = "END";
+  private SimpleArrayList names;
+  private SimpleArrayList times;
 
-	private SimpleArrayList names;
-	private SimpleArrayList times;
+  public Doodle() {
+    names = new SimpleArrayList();
+    times = new SimpleArrayList();
+  }
 
-	public Doodle() {
-		names = new SimpleArrayList();
-		times = new SimpleArrayList();
-	}
+  public static void main(String[] args) {
+    Doodle d = new Doodle();
+    d.askTime();
 
-	public static void main(String[] args) {
-		Doodle d = new Doodle();
-		d.askTime();
+    System.out.println(d);
+  }
 
-		System.out.println(d);
-	}
+  public String toTable() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(String.format(DOODLE_FORMAT, "  ----  ", "Time")); // 2 spaces, 4 dashes, 2 spaces
 
-	public String toTable() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(String.format("|%-10s|%-10s|%n", "  ----  ", "Time")); // 2 spaces, 4 dashes, 2 spaces
+    int len = names.size();
+    for (int i = 0; i < len; i++) {
+      sb.append(String.format(DOODLE_FORMAT, names.get(i), times.get(i)));
 
-		int len = names.size();
-		for (int i = 0; i < len; i++) {
-			sb.append(String.format("|%-10s|%-10d|%n", names.get(i), times.get(i)));
+    }
+    return sb.toString();
 
-		}
-		return sb.toString();
+  }
 
-	}
-
-	// Override method
-	@Override
+  // Override method
+  @Override
   public String toString() {
-		return this.toTable();
+    return this.toTable();
 
-	}
+  }
 
-	public void askTime() {
-		String line = null;
-		Scanner sc = new Scanner(System.in);
+  public void askTime() {
+    String line = null;
+    Scanner sc = new Scanner(System.in);
 
-		while (true) {
-			System.out.println("Tell me your name or tell me to END.");
-			line = sc.nextLine();
+    while (true) {
+      System.out.println("Tell me your name or tell me to END.");
+      line = sc.nextLine();
 
-			if (line.equals(EOI)) {
-				break;
-			}
+      if (line.equals(EOI)) {
+        break;
+      }
 
-			names.add(line);
-			System.out.println(line + ", when can you meet?");
-			line = sc.nextLine();
-			times.add(Integer.parseInt(line));
-		}
+      names.add(line);
+      String name = line;
+      int time = -1;
+      while(time < 0 || time > 23) {
+        System.out.println(name + ", when can you meet (0-23)?");
+        line = sc.nextLine();
+        time = Integer.parseInt(line);
+      }
 
-		sc.close();
+      times.add(Integer.valueOf(time));
+    }
 
-	}
+    sc.close();
 
-	public SimpleArrayList getNames() {
-		return names;
-	}
+  }
 
-	public SimpleArrayList getTimes() {
-		return times;
-	}
+  public SimpleArrayList getNames() {
+    return names;
+  }
+
+  public SimpleArrayList getTimes() {
+    return times;
+  }
 
 }
