88078ecf7447b934c204cf09086ec4260372e081
tjagust <tomislav.jagust@fer.hr>
2021-03-21 16:49:30 +0100

    Homework 04
    The initial commit
    
    And so it begins!

---
+++ Exercises/Homework-04/src/hr/fer/oop/homework_03/e06/CompetitionEntry.java
@@ -0,0 +1,63 @@
+package hr.fer.oop.homework_03.e06;
+
+import hr.fer.oop.homework_03.e01.Dessert;
+import hr.fer.oop.homework_03.e04.*;
+
+public class CompetitionEntry {
+	private Teacher teacher;
+	private Dessert dessert;	
+	private Student[] students;
+	private int[] ratings;
+	
+	//internal, for addRating
+	private int idx;
+	
+	public CompetitionEntry(Teacher teacher, Dessert dessert) {
+		this.teacher = teacher;
+		this.dessert = dessert;
+		this.students = new Student[3];
+		this.ratings = new int[3];
+		idx = 0;
+	}
+
+	public Teacher getTeacher() {
+		return teacher;
+	}
+
+	public Dessert getDessert() {
+		return dessert;
+	}
+
+	public Student[] getStudents() {
+		return students;
+	}
+
+	public int[] getRatings() {
+		return ratings;
+	}
+	
+	public boolean addRating (Student student, int rating) {
+		if (idx == 3) return false; //full
+		
+		for (Student s : students) {
+			if (s != null && s.equals(student))
+				return false; //already in
+		}
+		
+		students[idx] = student;
+		ratings[idx] = rating;
+		idx++;
+		return true;
+	}
+	
+	public double getRating() {
+		if (idx == 0) return 0;
+		
+		double sum = 0;
+		for (int i=0; i<idx; i++)
+			sum+= ratings[i];
+		
+		return sum/idx; 
+	} 
+	
+}
---
+++ Exercises/Homework-04/src/hr/fer/oop/homework_03/e10/Limo.java
@@ -0,0 +1,44 @@
+package hr.fer.oop.homework_03.e10;
+
+public class Limo extends Vehicle {
+
+	private double length;
+	private boolean miniBar;
+	private boolean sunRoof;
+	
+	public Limo(String regNo, String model, int year, double price, double length, boolean miniBar, boolean sunRoof) {
+		super(regNo, model, year, price);
+		this.length = length;
+		this.miniBar = miniBar;
+		this.sunRoof = sunRoof;
+	}
+
+	public double getLength() {
+		return length;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public boolean hasMiniBar() {
+		return miniBar;
+	}
+
+	public void setMiniBar(boolean miniBar) {
+		this.miniBar = miniBar;
+	}
+
+	public boolean hasSunRoof() {
+		return sunRoof;
+	}
+
+	public void setSunRoof(boolean sunRoof) {
+		this.sunRoof = sunRoof;
+	}	
+	
+	@Override
+	public double getPricePerDay() {
+		return this.getPrice()*1.5;
+	}
+}
