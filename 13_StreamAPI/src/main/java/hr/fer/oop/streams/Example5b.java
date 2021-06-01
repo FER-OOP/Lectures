c3c1355b9f2d4f6818eeb953ba369e0807619d81
Boris Milasinovic <boris.milasinovic@fer.hr>
2021-06-01 19:26:37 +0200

    collator example

--- 13_StreamAPI/src/main/java/hr/fer/oop/streams/Example5b.java
+++ 13_StreamAPI/src/main/java/hr/fer/oop/streams/Example5b.java
@@ -1,18 +1,21 @@
 package hr.fer.oop.streams;
 
+import java.text.Collator;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class Example5b {
 
 	public static void main(String[] args) {
-		List<Student> students = StudentData.loadCN();
-		
+		List<Student> students = StudentData.load();
+		Comparator<Object> comp = Collator.getInstance(Locale.forLanguageTag("hr"));
 		String lastNames = students
 							.stream()
 							.filter(s -> s.getPoints() > 30)
 							.map(s -> s.getLastName())		
-							.sorted()
+							.sorted(comp)
 							.collect(Collectors.joining(", "));
 		
 		
