26f955d520ad3b86a7fabf786383df7a98ddf0ce
Boris Milašinović <boris.milasinovic@fer.hr>
2022-02-26 12:08:06 +0100

    Upgrade Maven files for Java 17

--- 05_Enums/src/main/java/hr/fer/oop/enums/Main.java
+++ 05_Enums/src/main/java/hr/fer/oop/enums/Main.java
@@ -14,19 +14,31 @@ public class Main {
     }
   }
 
-  public static void printOppinion(AndroidVersion version) {
-    switch (version) {
-    case MARSHMALLOW:
-      System.out.println(" Marshmallow is too old version!");
-      break;
-    case ANDROID_10:
-    case ANDROID_11:
-      System.out.println(" " + version + " is too new version. Just few phones supports it!!!");
-      break;
-    default:
-      System.out.println(" " + version + " is OK.");
-      break;
-    }
-  }
+//	new Java 17 construct (does not require break, and allows multiple values per case)
+	public static void printOppinion(AndroidVersion version) {
+		switch (version) {
+		case MARSHMALLOW -> 
+			System.out.println(" Marshmallow is too old version!");
+		case ANDROID_10, ANDROID_11 -> 
+			System.out.println(" " + version + " is too new version. Just few phones supports it!!!");
+		default -> 
+			System.out.println(" " + version + " is OK.");
+		}
+	}
+//  
+//	public static void printOppinion(AndroidVersion version) {
+//		switch (version) {
+//		case MARSHMALLOW:
+//			System.out.println(" Marshmallow is too old version!");
+//			break;
+//		case ANDROID_10:
+//		case ANDROID_11:
+//			System.out.println(" " + version + " is too new version. Just few phones supports it!!!");
+//			break;
+//		default:
+//			System.out.println(" " + version + " is OK.");
+//			break;
+//		}
+//	}
 
 }
