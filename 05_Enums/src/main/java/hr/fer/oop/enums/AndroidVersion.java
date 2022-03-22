f19406607e838c287a755cc28e00c23187ceaac0
siddhxrthh <ksiddharth127@gmail.com>
2022-03-22 18:56:35 -0300

    refactoring: move printOpinion method to AndroidVersion

--- 05_Enums/src/main/java/hr/fer/oop/enums/AndroidVersion.java
+++ 05_Enums/src/main/java/hr/fer/oop/enums/AndroidVersion.java
@@ -18,4 +18,27 @@ public enum AndroidVersion {
     return version;
   }
 
+// new Java 17 construct (does not require break, and allows multiple values per case)
+  public static void printOpinion(AndroidVersion version){
+      switch (version){
+          case MARSHMALLOW -> System.out.println(" Marshmallow is too old version!");
+          case ANDROID_10, ANDROID_11 -> System.out.println(" " + version + " is too new version. Just few phones supports it!!!");
+          default -> System.out.println(" " + version + " is OK.");
+      }
+  }
+
+//	public static void printOpinion(AndroidVersion version) {
+//		switch (version) {
+//		case MARSHMALLOW:
+//			System.out.println("Marshmallow is too old version!");
+//			break;
+//		case ANDROID_10:
+//		case ANDROID_11:
+//			System.out.println(" " + version + " is too new version. Just few phones supports it!!!");
+//			break;
+//		default:
+//			System.out.println(" " + version + " is OK.");
+//			break;
+//		}
+//	}
 }
--- 05_Enums/src/main/java/hr/fer/oop/enums/Main.java
+++ 05_Enums/src/main/java/hr/fer/oop/enums/Main.java
@@ -10,35 +10,7 @@ public class Main {
 
     for(AndroidVersion v: AndroidVersion.values()) {
       System.out.println(v + " " + v.getVersion());
-      printOppinion(v);
+      AndroidVersion.printOpinion(v);
     }
   }
-
-//	new Java 17 construct (does not require break, and allows multiple values per case)
-	public static void printOppinion(AndroidVersion version) {
-		switch (version) {
-		case MARSHMALLOW -> 
-			System.out.println(" Marshmallow is too old version!");
-		case ANDROID_10, ANDROID_11 -> 
-			System.out.println(" " + version + " is too new version. Just few phones supports it!!!");
-		default -> 
-			System.out.println(" " + version + " is OK.");
-		}
-	}
-//  
-//	public static void printOppinion(AndroidVersion version) {
-//		switch (version) {
-//		case MARSHMALLOW:
-//			System.out.println(" Marshmallow is too old version!");
-//			break;
-//		case ANDROID_10:
-//		case ANDROID_11:
-//			System.out.println(" " + version + " is too new version. Just few phones supports it!!!");
-//			break;
-//		default:
-//			System.out.println(" " + version + " is OK.");
-//			break;
-//		}
-//	}
-
 }
